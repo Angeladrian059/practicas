3b3267d86b1714621f263fdb9a7f224e0c24c62f
Angeladrian059 <angeladriand059@gmail.com>
2026-08-06 03:14:20 -0600

    Se agrego el constructor alumno

--- practicaEj/src/modelos/Alumno.java
+++ practicaEj/src/modelos/Alumno.java
@@ -1,10 +1,28 @@
-package modelos;
+package models;
+
+import database.Conexion;
+
+import java.sql.*;
+import java.util.ArrayList;
 
 public class Alumno {
+
     private int id;
     private String matricula;
     private String nombre;
     private int edad;
     private String sexo;
     private String correo;
+
+    public Alumno() {
+    }
+
+    // Constructor sin id
+    public Alumno(String matricula, String nombre, int edad, String sexo, String correo) {
+        this.matricula = matricula;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = sexo;
+        this.correo = correo;
+    }
 }
