32e42227d26476e4feef003952e6f194115314c3
Angeladrian059 <angeladriand059@gmail.com>
2026-08-06 08:54:35 -0600

    Creacion del PlanBasico

---
+++ practicaEj4/src/estrategias/PlanBasico.java
@@ -0,0 +1,12 @@
+package estrategias;
+
+public class PlanBasico implements PlanSuscripcion {
+
+    @Override
+    public double calcularCosto(int meses) {
+
+        return meses * 5.0;
+
+    }
+
+}
