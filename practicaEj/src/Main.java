4b39ee55315902591bf09a7bb1de128d9434d5ec
Angeladrian059 <angeladriand059@gmail.com>
2026-08-06 04:00:48 -0600

    creacion del menu

--- practicaEj/src/Main.java
+++ practicaEj/src/Main.java
@@ -11,6 +11,19 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         int opcion;
+        do {
+
+            System.out.println("\n========== MENU ==========");
+            System.out.println("1. Registrar alumno");
+            System.out.println("2. Mostrar alumnos");
+            System.out.println("3. Actualizar alumno");
+            System.out.println("4. Eliminar alumno");
+            System.out.println("5. Contar hombres y mujeres");
+            System.out.println("6. Salir");
+            System.out.print("Seleccione una opción: ");
+
+            opcion = sc.nextInt();
+            sc.nextLine();
 
     }
 }
