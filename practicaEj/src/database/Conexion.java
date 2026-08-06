3036f9a0527362b0eee07dececfa699f698e78e4
Angeladrian059 <angeladriand059@gmail.com>
2026-08-06 04:26:03 -0600

    atributos y importaciones de conxion con el metodo conectar

--- practicaEj/src/database/Conexion.java
+++ practicaEj/src/database/Conexion.java
@@ -1,4 +1,17 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexion {
-}
+
+    private static final String SERVER = "localhost";
+    private static final String PORT = "1433";
+    private static final String DATABASE = "Escuela";
+    private static final String USER = "sa";
+    private static final String PASSWORD = "TU_CONTRASEÑA";
+
+    public static Connection conectar() {
+
+        Connection conexion = null;
