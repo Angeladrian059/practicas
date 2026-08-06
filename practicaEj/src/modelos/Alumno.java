ba5e3bd460f656db11dc6019ea712b0fc4c015b3
Angeladrian059 <angeladriand059@gmail.com>
2026-08-06 03:16:19 -0600

    Se agregaron los getters y setters :p

--- practicaEj/src/modelos/Alumno.java
+++ practicaEj/src/modelos/Alumno.java
@@ -25,4 +25,53 @@ public class Alumno {
         this.sexo = sexo;
         this.correo = correo;
     }
+    // Getters y Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
 }
