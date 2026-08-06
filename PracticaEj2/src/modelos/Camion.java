59dab69467501fc0b82dba5f1d43634d9fcfac39
Angeladrian059 <angeladriand059@gmail.com>
2026-08-06 06:03:39 -0600

    Terminacion de detalles que tenia

--- PracticaEj2/src/modelos/Camion.java
+++ PracticaEj2/src/modelos/Camion.java
@@ -1,4 +1,73 @@
 package modelos;
 
-public class Camion {
+
+import tarifas.Tarifa;
+
+
+
+public class Camion extends Vehiculo {
+
+
+    private double capacidadCarga;
+
+    private int numeroEjes;
+
+
+
+
+    public Camion(
+            String placa,
+            String marca,
+            String modelo,
+            String color,
+            int anio,
+            String propietario,
+            int horas,
+            Tarifa tarifa,
+            double capacidadCarga,
+            int numeroEjes
+    ){
+
+
+        super(
+                placa,
+                marca,
+                modelo,
+                color,
+                anio,
+                propietario,
+                horas,
+                tarifa
+        );
+
+
+        this.capacidadCarga = capacidadCarga;
+
+        this.numeroEjes = numeroEjes;
+
+
+    }
+
+
+
+
+    @Override
+    public String getTipo(){
+
+        return "Camion";
+
+    }
+
+
+
+
+
+    public double getCapacidadCarga(){
+
+        return capacidadCarga;
+
+    }
+
+
+
 }
