266e57939dca6e6200da2de1ceeedb4ca9a5f2db
Angeladrian059 <angeladriand059@gmail.com>
2026-08-06 03:55:36 -0600

    imports del main

--- practicaEj/src/Main.java
+++ practicaEj/src/Main.java
@@ -1,5 +1,16 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
+import modelos.Alumno;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+
+    }
 }
